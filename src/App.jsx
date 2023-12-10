9583259bf8aacc1edb3c3b957b9ab05b7893f48a
AlexKomanov <akomanov88@gmail.com>
2023-12-10 06:12:19 +0200

    adding header component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Header} from "./Header.jsx";
+
+function App() {
+
+
+  return (
+    <div className="app">
+      <Header />
+    <h1>Building Gmail</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,39 @@
+import './Header.css'
+import MenuIcon from '@mui/icons-material/menu'
+import {Avatar, IconButton} from "@mui/material";
+import logo from '../public/mail-logo.png'
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+export function Header(props) {
+    return (
+        <div className="header">
+            <div className="header_left">
+                <IconButton>
+                    <MenuIcon/>
+                </IconButton>
+                <img src={logo} alt="email logo"/>
+            </div>
+            <div className="header_middle">
+                <SearchIcon />
+                <input placeholder="Search for email" type="text"/>
+                <ArrowDropDownIcon className="header_inputCaret"/>
+            </div>
+
+            <div className="header_right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon/>
+                </IconButton>
+                <Avatar/>
+
+            </div>
+
+        </div>
+    );
+}
+
