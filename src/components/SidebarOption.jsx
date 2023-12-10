db77ad4d8856f88fee95d73ad2f78f6736f4ddb0
AlexKomanov <akomanov88@gmail.com>
2023-12-10 21:19:47 +0200

    adding sidebar sections

---
+++ src/components/SidebarOption.jsx
@@ -0,0 +1,11 @@
+import './SidebarOption.css';
+export function SidebarOption({ Icon, title, number, selected}) {
+    return (
+        <div className={`sidebarOption ${selected && 'sidebarOption-active'}`}>
+            <Icon />
+            <h3>{title}</h3>
+            <p>{number}</p>
+        </div>
+    );
+}
+
