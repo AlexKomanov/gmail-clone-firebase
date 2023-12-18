d0ff2be03c47d98d78715156053681d4fb6d4679
AlexKomanov <akomanov88@gmail.com>
2023-12-18 17:23:29 +0200

    adding icons inside email component

--- src/components/Mail.jsx
+++ src/components/Mail.jsx
@@ -1,8 +1,58 @@
 import './Mail.css'
+import {IconButton} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import {
+    ArrowBack,
+    CheckCircle,
+    Delete,
+    Email,
+    Error,
+    LabelImportant, MoreVert,
+    MoveToInbox,
+    WatchLater
+} from "@mui/icons-material";
 
 export function Mail() {
+
+    const navigate = useNavigate();
     return (
-        <div className="mail">Mail Component</div>
+        <div className="mail">
+            <div className="mail_tools">
+                <div className="mail_toolsLeft">
+                    <IconButton onClick={() => navigate("/gmail-clone-firebase/")}>
+                        <ArrowBack/>
+                    </IconButton>
+                    <IconButton >
+                        <MoveToInbox/>
+                    </IconButton>
+                    <IconButton >
+                        <Error/>
+                    </IconButton>
+                    <IconButton >
+                        <Delete/>
+                    </IconButton>
+                    <IconButton >
+                        <Email/>
+                    </IconButton>
+                    <IconButton >
+                        <WatchLater/>
+                    </IconButton>
+                    <IconButton >
+                        <CheckCircle/>
+                    </IconButton>
+                    <IconButton >
+                        <LabelImportant/>
+                    </IconButton>
+                    <IconButton >
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+                <div className="mail_toolsRight">
+
+                </div>
+            </div>
+            <h1>Mail Component</h1>
+        </div>
     );
 }
 
