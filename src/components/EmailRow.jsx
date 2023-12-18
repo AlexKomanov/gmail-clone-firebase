efc355cd9f4f4e2f6a4546875ea882ba59541ba4
AlexKomanov <akomanov88@gmail.com>
2023-12-18 16:39:38 +0200

    adding forward to the email component

--- src/components/EmailRow.jsx
+++ src/components/EmailRow.jsx
@@ -1,10 +1,14 @@
 import {Checkbox, IconButton} from "@mui/material";
 import {LabelImportantOutlined, StarBorderOutlined} from "@mui/icons-material";
 import "./EmailRow.css"
+import {useNavigate} from "react-router-dom";
+
 
 export function EmailRow({id, title, subject, description, time}) {
+
+    const navigate = useNavigate()
     return (
-        <div className="emailRow">
+        <div onClick={() => navigate("/gmail-clone-firebase/mail")} className="emailRow">
             <div className="emailRow_options">
                 <Checkbox/>
                 <IconButton>
